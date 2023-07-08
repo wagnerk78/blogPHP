5b8767e004e70a4c97a9df9d965a1099aa77ea4f
wagnerk78 <wagnerkroiss@gmail.com>
2023-07-07 21:46:49 -0300

    07th July

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,15 @@
 <?php
     include_once("helpers/url.php");
+    include_once("data/posts.php");
+    include_once("data/categories.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
